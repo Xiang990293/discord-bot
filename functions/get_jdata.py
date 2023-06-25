bb8be55d61b8e8c393614c233f83ef19276cc4d3
Xiang990293 <990293kwi@gmail.com>
2023-06-25 21:20:59 +0800

    okay, I'm suck...

--- functions/get_jdata.py
+++ functions/get_jdata.py
@@ -11,5 +11,5 @@ def get_jdata(mode = 1):
 			return json.load(jfile)
 		else:
 			key = [i for i in jfile]
-			value = [jfile for j in key]
-			return {key[i]:str(os.environ.get(value[i])) for i in range(len(key))}
+			value = [jfile[j] for j in key]
+			return {key[i]:os.environ.get(value[i]) for i in range(len(key))}
