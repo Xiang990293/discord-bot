7c0322361df2fd7d7eb971441495ee2a8e6a806e
Xiang990293 <990293kwi@gmail.com>
2023-06-25 22:10:18 +0800

    ctx.delete() -> ctx.message.delete()

--- cmds/Main.py
+++ cmds/Main.py
@@ -16,22 +16,22 @@ class Main(Cog_Extension):
 	
 	@commands.command(name="ping", alienses=["pinglagecy"], help="顯示延遲毫秒數")
 	async def pinglatency(self, ctx):
-		await ctx.delete()
+		await ctx.message.delete()
 		await ctx.send(f'{round(self.bot.latency*1000)} (ms)', delete_after=30.0)
 
 	@commands.command()
 	async def picture(self, ctx, *arg):
 		if arg == ():
-			await ctx.delete()
+			await ctx.message.delete()
 			await ctx.send(file=discord.File("NoURL.png"))
 		else:
-			await ctx.delete()
+			await ctx.message.delete()
 			pic = arg[0]
 			await ctx.send(pic)
 
 	@commands.command()
 	async def test(self, ctx):
-		await ctx.delete()
+		await ctx.message.delete()
 		await ctx.send(f"{ctx.message.channel}, I am here!")
 
 	@commands.command(name="delete", aliases=["del", "clc"], help="刪除特定數量的訊息，默認為 1 個")
